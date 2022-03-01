3800af3a2a47ff6447beee0a0051f385d2fdc746
hamed <shiani@outlook.com>
2022-03-01 10:46:15 +0330

    add django rest framework and create project app

--- HamedShiani/settings.py
+++ HamedShiani/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'rest_framework',
+
+    # my apps
+    'project',
 ]
 
 MIDDLEWARE = [
