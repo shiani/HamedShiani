cafb36d1000b984ec72edad62c42746efab6c284
hamed <shiani@outlook.com>
2022-03-07 11:03:53 +0330

    add search to project list app

--- project/views.py
+++ project/views.py
@@ -9,6 +9,8 @@ from .seializers import ProjectSerializer
 class ProjectList(generics.ListAPIView):
     serializer_class = ProjectSerializer
     permission_classes = (AllowAny,)
+    search_fields = ["title", "client_name", "client_services", "objective", "challenges", "category__name",
+                     "tools__name"]
 
     def get_queryset(self):
         return Project.objects.filter(is_active=True)
